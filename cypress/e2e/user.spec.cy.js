7ae456202e9584633c42acab174368c314095bf4
José Augusto <jose.augusto.n.borges@gmail.com>
2025-05-07 23:35:38 -0300

    Add myInfo in Page

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,30 +2,14 @@ import userData from '../fixtures/users/user-data.json'
 import LoginPage from '../pages/loginPage.js'
 import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
+import MyInfoPage from '../pages/myInfoPage.js'
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
+const myInfoPage = new MyInfoPage();
 
 describe('template spec', () => {
-
-  //Criando um unico seletor para utilizar em varias partes do codigo
-  const selectorsList = {
-
-   
-    firstNameField: "[name='firstName']",
-    lastNameField: "[name='lastName']",
-    genericField: '.oxd-input--active',
-    gereircDateField: "[placeholder='yyyy-dd-mm']",
-    dateClosedButton: '.--close',
-    submitButton: "[type='submit']",
-    selectButton: '.oxd-select-text--after',
-    selectNationality: ':nth-child(27)',
-    selectMaritalStatus: '.oxd-select-dropdown > :nth-child(4)',
-    selectBloodType: '.oxd-select-dropdown > :nth-child(8)'
-   
-
-  }
 
   it.only('CT01 User info update - Sucess', () => {
 
@@ -35,33 +19,15 @@ describe('template spec', () => {
     dashboardPage.accessDashboard()
     dashboardPage.validationDashboard()
 
-    menuPage.accessMyInfo();
+    menuPage.accessMyInfo()
 
-
-
-    // cy.get(selectorsList.myInfoButton).click();
-    // cy.get(selectorsList.firstNameField).clear().type('firstNameField')
-    // cy.get(selectorsList.lastNameField).clear().type('lastNameField')
-    // cy.get(selectorsList.genericField).eq(3).clear().type('EmployId')
-    // cy.get(selectorsList.genericField).eq(4).clear().type('Other Id')
-    // cy.get(selectorsList.genericField).eq(5).clear().type('Drivers License Number')
-    // cy.get(selectorsList.gereircDateField).eq(1).clear().type('2025-05-05')
-    // cy.get(selectorsList.dateClosedButton).click()
-    // cy.get(selectorsList.submitButton).eq(0).click()
-
-
-    // cy.get('body').should('contain', 'Successfully Updated')
-    // cy.get('.oxd-toast-close').click();
-
-    // cy.get(selectorsList.selectButton).eq(0).click()
-    // cy.get(selectorsList.selectNationality).click()
-
-    // cy.get(selectorsList.selectButton).eq(1).click()
-    // cy.get(selectorsList.selectMaritalStatus).click()
-
-    // cy.get(selectorsList.selectButton).eq(2).click()
-    // cy.get(selectorsList.selectBloodType).click()
-
+    myInfoPage.myInfoName()
+    myInfoPage.myInfoId()
+    myInfoPage.myInfoDate()
+    myInfoPage.myInfoButton()
+    myInfoPage.myInfoTextSuccess()
+    myInfoPage.myInfoSelect()
+ 
 
   })
 
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,68 @@
+class myInfoPage{
+    selectorsList(){
+          //Criando um unico seletor para utilizar em varias partes do codigo
+        const selectors = {
+            //Formulario de nomes
+            firstNameField: "[name='firstName']",
+            lastNameField: "[name='lastName']",
+
+            //Formulario generico
+            genericField: '.oxd-input--active',
+            gereircDateField: "[placeholder='yyyy-dd-mm']",
+
+            //Botões
+            dateClosedButton: '.--close',
+            submitButton: "[type='submit']",
+
+            //Selecionar
+            selectButton: '.oxd-select-text--after',
+            selectNationality: ':nth-child(27)',
+            selectMaritalStatus: '.oxd-select-dropdown > :nth-child(4)',
+            selectBloodType: '.oxd-select-dropdown > :nth-child(8)'
+        }
+        return selectors
+    }
+
+    myInfoName(){
+        cy.get(this.selectorsList().firstNameField).clear().type('Ana')
+        cy.get(this.selectorsList().lastNameField).clear().type('Nunes')
+       
+    }
+
+    myInfoId(){
+        cy.get(this.selectorsList().genericField).eq(3).clear().type('TesteFunci')
+        cy.get(this.selectorsList().genericField).eq(4).clear().type('TesteOutros')
+        cy.get(this.selectorsList().genericField).eq(5).clear().type('Drivers License Number')
+
+    }
+
+    myInfoDate(){
+        cy.get(this.selectorsList().gereircDateField).eq(1).clear().type('2025-05-05')
+        cy.get(this.selectorsList().dateClosedButton).click()
+
+    }
+
+    myInfoButton(){
+        cy.get(this.selectorsList().submitButton).eq(0).click()
+        
+    }
+    myInfoTextSuccess(){
+        cy.get('body').should('contain', 'Successfully Updated')
+        cy.get('.oxd-toast-close').click();
+    }
+
+    myInfoSelect(){
+        cy.get(this.selectorsList().selectButton).eq(0).click()
+        cy.get(this.selectorsList().selectNationality).click()
+
+        cy.get(this.selectorsList().selectButton).eq(1).click()
+        cy.get(this.selectorsList().selectMaritalStatus).click()
+
+        cy.get(this.selectorsList().selectButton).eq(2).click()
+        cy.get(this.selectorsList().selectBloodType).click()
+
+    }
+
+}
+
+export default myInfoPage
